d47ce6c4be27fc24e45059161d09809310a53442
Jordan Alphonso <jordanalphonso1@yahoo.com>
2018-03-03 10:02:00 -0600

    modify init

---
+++ nbaTrends/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+    Utils
+
+    Root package for all Utility Classes and Functions
+"""
+
+__author__ = "Jordan Alphonso"
+__copyright__ = "Copyright 2018, jordanalphonso.net"
+__credits__ = ["Jordan Alphonso"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Jordan Alphonso"
+__email__ = "jordanalphonso1@yahoo.com"
+
+import nbaTrends.utils.config_utils
