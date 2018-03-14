3111bc442a248425f883b55685dc6998f0249f7e
Jordan Alphonso <jordanalphonso1@yahoo.com>
2018-03-14 15:11:44 -0500

    configured logstash

--- nbaTrends-hadoop/docker/entrypoint.sh
+++ nbaTrends-hadoop/docker/entrypoint.sh
@@ -7,10 +7,11 @@ if [ "$1" == "master" ]; then
     # start all hadoop services on master node
     hdfs namenode -format
     $HADOOP_HOME/sbin/start-all.sh
-    sleep infinity
 else 
     # start slave and connect to master
     hdfs datanode -format
     hdfs --daemon start datanode
-    sleep infinity
 fi
+
+# start filebeat
+/usr/share/filebeat/filebeat -e -c /usr/share/filebeat/filebeat.yml
