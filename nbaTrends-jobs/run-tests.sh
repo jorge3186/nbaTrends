1b49c49f992b5f7f37d0e3f25400d350baec7028
Jordan Alphonso <jordanalphonso1@yahoo.com>
2018-03-21 18:28:36 -0500

    avro testing

--- nbaTrends-jobs/run-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -v -j -k -L -O 'http://apache.claz.org/avro/avro-1.8.2/py3/avro-python3-1.8.2.tar.gz'
-tar -xzf avro-python3-1.8.2.tar.gz
-python ./avro-python3-1.8.2/setup.py install
-
-python -m unittest discover nbaTrends-jobs
